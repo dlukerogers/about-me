ed35d44d0ce54094fa4f37d8411b164c8aac6d41
Daniel Luke Rogers <dluke.rogers@gmail.com>
2023-06-07 19:53:36 -0500

    lab 3

--- README.md
+++ README.md
@@ -14,8 +14,10 @@ I create a website that is all about me! I include my bio and other things about
 ### Lighthouse Accessibility Report Score
 
 * ![Lighthouse report - Lab 2](/img/Lighthouse-report-lab2.png "Lighthouse report - Lab 2")
+* ![Lighthouse report - Lab 3](/img/Lighthouse-report-lab3.png "Lighthouse report - Lab 3")
 
 ### Reflections and Comments
 
-* Consider including the answers to your daily journal and submission questions here
-* This is also a good place to reflect on the tools and resources used and learned
+#### Learning Journal: 02
+
+I see myself planning and evaluating my learning by being organized with my notes and staying focused on the lecture or lab. Today I felt like my notes were scattered between different places such as on replit, vs code, or my notebook. Finding a good singular place to put my notes during the lectures will help me stay organized and improve my learning. I also think staying focused on the task at hand, whether that's the lecture, the lab, or the reading, and not getting distracted by external things will help my learning as well. I did well today in that regard. I also think asking questions and finding the finer details of a concept will really help my learning as well. I'm hoping to evaluate my learning once a day to know if I did a good job learning the material taught that day. It is really interesting that adults are 100% in control of their learning. I find it so true that adults are motivated by intrinsic factors. Before being a student at Code Fellows, I was definitely motivated by extrinsic factors such as graduating and getting good grades. I was not motivated by my curiosity or passion or desire to use what I learn someday in the future.  
--- js/app.js
+++ js/app.js
@@ -20,6 +20,14 @@ if(doIHavePets.toLowerCase() === 'yes' || doIHavePets.toLowerCase() === 'y') {
   alert('You need to answer with a yes or a no');
 }
 
+let question1CorrectAnswer = 0;
+
+if (doIHavePets.toLowerCase() === 'no' || doIHavePets.toLowerCase() === 'n') {
+  question1CorrectAnswer = 1;
+} else {
+  question1CorrectAnswer = 0;
+}
+
 let didILiveInColorado = prompt('Did I live in Colorado? Yes or no?');
 
 if(didILiveInColorado.toLowerCase() === 'yes' || didILiveInColorado.toLowerCase() === 'y') {
@@ -31,6 +39,14 @@ if(didILiveInColorado.toLowerCase() === 'yes' || didILiveInColorado.toLowerCase(
 } else {
   //console.log('You need to answer with a yes or a no');
   alert('You need to answer with a yes or a no');
+}
+
+let question2CorrectAnswer = 0;
+
+if (didILiveInColorado.toLowerCase() === 'yes' || didILiveInColorado.toLowerCase() === 'y') {
+  question2CorrectAnswer = 1;
+} else {
+  question2CorrectAnswer = 0;
 }
 
 let doIWantToBeAstronaut = prompt('Do I want to be an astronaut someday? Yes or no?');
@@ -46,6 +62,14 @@ if(doIWantToBeAstronaut.toLowerCase() === 'yes' || doIWantToBeAstronaut.toLowerC
   alert('You need to answer with a yes or a no');
 }
 
+let question3CorrectAnswer = 0;
+
+if (doIWantToBeAstronaut.toLowerCase() === 'no' || doIWantToBeAstronaut.toLowerCase() === 'n') {
+  question3CorrectAnswer = 1;
+} else {
+  question3CorrectAnswer = 0;
+}
+
 let didIGoToUW = prompt('Did I go to the University of Washington? Yes or no?');
 
 if(didIGoToUW.toLowerCase() === 'yes' || didIGoToUW.toLowerCase() === 'y') {
@@ -57,6 +81,14 @@ if(didIGoToUW.toLowerCase() === 'yes' || didIGoToUW.toLowerCase() === 'y') {
 } else {
   //console.log('You need to answer with a yes or a no');
   alert('You need to answer with a yes or a no');
+}
+
+let question4CorrectAnswer = 0;
+
+if (didIGoToUW.toLowerCase() === 'no' || didIGoToUW.toLowerCase() === 'n') {
+  question4CorrectAnswer = 1;
+} else {
+  question4CorrectAnswer = 0;
 }
 
 let doILoveSports = prompt('Do I love sports? Yes or no?');
@@ -71,5 +103,94 @@ if(doILoveSports.toLowerCase() === 'yes' || doILoveSports.toLowerCase() === 'y')
   //console.log('You need to answer with a yes or a no');
   alert('You need to answer with a yes or a no');
 }
+
+let question5CorrectAnswer = 0;
+
+if (doILoveSports.toLowerCase() === 'yes' || doILoveSports.toLowerCase() === 'y') {
+  question5CorrectAnswer = 1;
+} else {
+  question5CorrectAnswer = 0;
+}
+
+let guessNumber = prompt('Guess a number between 1 and 10');
+guessNumber = parseInt(guessNumber);
+
+let guessNumberAttempts = 4;
+
+while (guessNumberAttempts) {
+  if(guessNumber > 2) {
+    alert(`That number is too high. You have ${guessNumberAttempts} attempts remaining.`);
+    guessNumber = prompt('Try again. Guess a number between 1 and 10');
+    guessNumber = parseInt(guessNumber);
+  } else if(guessNumber < 2) {
+    alert(`That number is too low. You have ${guessNumberAttempts} attempts remaining.`);
+    guessNumber = prompt('Try again. Guess a number between 1 and 10');
+    guessNumber = parseInt(guessNumber);
+  } else if(guessNumber === 2) {
+    alert('Congratulations! You are correct!');
+    break;
+  } else if (isNaN (guessNumber)) {
+    alert('Invalid answer!');
+    guessNumber = prompt('Try again. Guess a number between 1 and 10');
+  }
+  guessNumberAttempts--;
+}
+console.log (guessNumber);
+if (guessNumberAttempts <= 0) {
+  alert('You have run out of attempts! The corrrect answer was 2.');
+}
+
+let question6CorrectAnswer = 0;
+
+if (guessNumber === 2) {
+  question6CorrectAnswer = 1;
+} else {
+  question6CorrectAnswer = 0;
+}
+
+let topTenShows = ['The Office', 'Game of Thrones', 'Parks and Rec', 'Severence', 'Lost', 'New Girl', 'Friends', 'The Umbrella Academy', 'Band of Brothers', 'Friday Night Lights'];
+
+
+let guessShowAttempts = 6;
+let userShowGuess;
+
+while (guessShowAttempts > 0) {
+  userShowGuess = prompt('What is one of my favorite shows?');
+  let isGuessCorrect = false;
+  for (let i = 0; i < topTenShows.length; i++) {
+    // console.log(userShowGuess, topTenShows[i], userShowGuess.toLowerCase() === topTenShows[i].toLowerCase());
+    if (userShowGuess.toLowerCase() === topTenShows[i].toLowerCase()) {
+      isGuessCorrect = true;
+      break;
+    }
+    // userShowGuess = prompt('Try again. What is one of my favorite shows?');
+  }
+  if (isGuessCorrect) {
+    alert('Correct! That show is in my top ten!');
+    break;
+  } else {
+    guessShowAttempts--;
+    alert(`Incorrect. That is not in my top ten. You have ${guessShowAttempts} attempts remaining.`);
+  }
+  // if (!boolValue) {
+  //   break;
+  // }
+  // guessShowAttempts--;
+}
+// if (!guessShowAttempts) {
+//   alert('You have no more attempts remaining');
+// }
+
+let question7CorrectAnswer = 0;
+
+if (userShowGuess === topTenShows) {
+  question7CorrectAnswer = 1;
+} else {
+  question7CorrectAnswer = 0;
+}
+
+let score = alert(`You recieved a score of ${question1CorrectAnswer + question2CorrectAnswer + question3CorrectAnswer + question4CorrectAnswer + question5CorrectAnswer + question6CorrectAnswer + question7CorrectAnswer} out of 7 possible points`);
+
+score;
 
 alert('You may now enter my website, ' + userName + '.');
