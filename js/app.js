47cc92b8998ac32d6be3e8da075d1cb187788161
Daniel Luke Rogers <dluke.rogers@gmail.com>
2023-06-08 16:17:28 -0500

    cleaned up JS, added journal entry 3 to readme

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 // proof life
 // console.log('hi');
 
+let score = 0;
+
 let userName = prompt('What is your name?');
 
 alert('Hi ' + userName + '.' + ' Welcome to my website!');
@@ -15,24 +17,26 @@ if(doIHavePets.toLowerCase() === 'yes' || doIHavePets.toLowerCase() === 'y') {
 } else if (doIHavePets.toLowerCase() === 'no' || doIHavePets.toLowerCase() === 'n') {
   //console.log('Correct! I do not have any pets.');
   alert('Correct! I do not have any pets.');
+  score++
 } else {
   //console.log('You need to answer with a yes or a no');
   alert('You need to answer with a yes or a no');
 }
 
-let question1CorrectAnswer = 0;
+// let question1CorrectAnswer = 0;
 
-if (doIHavePets.toLowerCase() === 'no' || doIHavePets.toLowerCase() === 'n') {
-  question1CorrectAnswer = 1;
-} else {
-  question1CorrectAnswer = 0;
-}
+// if (doIHavePets.toLowerCase() === 'no' || doIHavePets.toLowerCase() === 'n') {
+//   question1CorrectAnswer = 1;
+// } else {
+//   question1CorrectAnswer = 0;
+// }
 
 let didILiveInColorado = prompt('Did I live in Colorado? Yes or no?');
 
 if(didILiveInColorado.toLowerCase() === 'yes' || didILiveInColorado.toLowerCase() === 'y') {
   //console.log('Correct! I did live in Colorado.');
   alert('Correct! I did live in Colorado.');
+  score++;
 } else if (didILiveInColorado.toLowerCase() === 'no' || didILiveInColorado.toLowerCase() === 'n') {
   //console.log('That is incorrect. I did live in Colorado.');
   alert('That is incorrect. I did live in Colorado.');
@@ -41,13 +45,13 @@ if(didILiveInColorado.toLowerCase() === 'yes' || didILiveInColorado.toLowerCase(
   alert('You need to answer with a yes or a no');
 }
 
-let question2CorrectAnswer = 0;
+// let question2CorrectAnswer = 0;
 
-if (didILiveInColorado.toLowerCase() === 'yes' || didILiveInColorado.toLowerCase() === 'y') {
-  question2CorrectAnswer = 1;
-} else {
-  question2CorrectAnswer = 0;
-}
+// if (didILiveInColorado.toLowerCase() === 'yes' || didILiveInColorado.toLowerCase() === 'y') {
+//   question2CorrectAnswer = 1;
+// } else {
+//   question2CorrectAnswer = 0;
+// }
 
 let doIWantToBeAstronaut = prompt('Do I want to be an astronaut someday? Yes or no?');
 
@@ -57,18 +61,19 @@ if(doIWantToBeAstronaut.toLowerCase() === 'yes' || doIWantToBeAstronaut.toLowerC
 } else if (doIWantToBeAstronaut.toLowerCase() === 'no' || doIWantToBeAstronaut.toLowerCase() === 'n') {
   //console.log('Correct! I want to be a Team Leader at a big tech company.');
   alert('Correct! I want to be a Team Leader at a big tech company.');
+  score++;
 } else {
   //console.log('You need to answer with a yes or a no');
   alert('You need to answer with a yes or a no');
 }
 
-let question3CorrectAnswer = 0;
+// let question3CorrectAnswer = 0;
 
-if (doIWantToBeAstronaut.toLowerCase() === 'no' || doIWantToBeAstronaut.toLowerCase() === 'n') {
-  question3CorrectAnswer = 1;
-} else {
-  question3CorrectAnswer = 0;
-}
+// if (doIWantToBeAstronaut.toLowerCase() === 'no' || doIWantToBeAstronaut.toLowerCase() === 'n') {
+//   question3CorrectAnswer = 1;
+// } else {
+//   question3CorrectAnswer = 0;
+// }
 
 let didIGoToUW = prompt('Did I go to the University of Washington? Yes or no?');
 
@@ -78,24 +83,26 @@ if(didIGoToUW.toLowerCase() === 'yes' || didIGoToUW.toLowerCase() === 'y') {
 } else if (didIGoToUW.toLowerCase() === 'no' || didIGoToUW.toLowerCase() === 'n') {
   //console.log('Correct! I went to Wheaton College.');
   alert('Correct! I went to Wheaton College.');
+  score++;
 } else {
   //console.log('You need to answer with a yes or a no');
   alert('You need to answer with a yes or a no');
 }
 
-let question4CorrectAnswer = 0;
+// let question4CorrectAnswer = 0;
 
-if (didIGoToUW.toLowerCase() === 'no' || didIGoToUW.toLowerCase() === 'n') {
-  question4CorrectAnswer = 1;
-} else {
-  question4CorrectAnswer = 0;
-}
+// if (didIGoToUW.toLowerCase() === 'no' || didIGoToUW.toLowerCase() === 'n') {
+//   question4CorrectAnswer = 1;
+// } else {
+//   question4CorrectAnswer = 0;
+// }
 
 let doILoveSports = prompt('Do I love sports? Yes or no?');
 
 if(doILoveSports.toLowerCase() === 'yes' || doILoveSports.toLowerCase() === 'y') {
   //console.log('Correct! I do love sports.');
   alert('Correct! I do love sports.');
+  score++;
 } else if (doILoveSports.toLowerCase() === 'no' || doILoveSports.toLowerCase() === 'n') {
   //console.log('That is incorrect. I do love sports.');
   alert('That is incorrect. I do love sports.');
@@ -104,93 +111,91 @@ if(doILoveSports.toLowerCase() === 'yes' || doILoveSports.toLowerCase() === 'y')
   alert('You need to answer with a yes or a no');
 }
 
-let question5CorrectAnswer = 0;
+// let question5CorrectAnswer = 0;
 
-if (doILoveSports.toLowerCase() === 'yes' || doILoveSports.toLowerCase() === 'y') {
-  question5CorrectAnswer = 1;
-} else {
-  question5CorrectAnswer = 0;
-}
+// if (doILoveSports.toLowerCase() === 'yes' || doILoveSports.toLowerCase() === 'y') {
+//   question5CorrectAnswer = 1;
+// } else {
+//   question5CorrectAnswer = 0;
+// }
 
-let guessNumber = prompt('Guess a number between 1 and 10');
-guessNumber = parseInt(guessNumber);
 
 let guessNumberAttempts = 4;
+let userHasGuessedCorrectly = false;
+let guessNumber = 0;
 
-while (guessNumberAttempts) {
+while (guessNumberAttempts && !userHasGuessedCorrectly) {
+  guessNumberAttempts--;
+  let message = 'Guess a number between 1 and 10';
+  if (guessNumberAttempts < 3) {
+    message = 'Try again. Guess a number between 1 and 10';
+  }
+  let guessNumber = parseInt(prompt(message));
+  // guessNumber = parseInt(guessNumber);
   if(guessNumber > 2) {
     alert(`That number is too high. You have ${guessNumberAttempts} attempts remaining.`);
-    guessNumber = prompt('Try again. Guess a number between 1 and 10');
-    guessNumber = parseInt(guessNumber);
+    // guessNumber = prompt('Try again. Guess a number between 1 and 10');
+    // guessNumber = parseInt(guessNumber);
   } else if(guessNumber < 2) {
     alert(`That number is too low. You have ${guessNumberAttempts} attempts remaining.`);
-    guessNumber = prompt('Try again. Guess a number between 1 and 10');
-    guessNumber = parseInt(guessNumber);
+    // guessNumber = prompt('Try again. Guess a number between 1 and 10');
+    // guessNumber = parseInt(guessNumber);
   } else if(guessNumber === 2) {
     alert('Congratulations! You are correct!');
-    break;
+    score++;
+    userHasGuessedCorrectly = true;
+    // break;
   } else if (isNaN (guessNumber)) {
     alert('Invalid answer!');
-    guessNumber = prompt('Try again. Guess a number between 1 and 10');
+    // guessNumber = prompt('Try again. Guess a number between 1 and 10');
   }
-  guessNumberAttempts--;
 }
 console.log (guessNumber);
 if (guessNumberAttempts <= 0) {
   alert('You have run out of attempts! The corrrect answer was 2.');
 }
 
-let question6CorrectAnswer = 0;
+// let question6CorrectAnswer = 0;
 
-if (guessNumber === 2) {
-  question6CorrectAnswer = 1;
-} else {
-  question6CorrectAnswer = 0;
-}
+// if (guessNumber === 2) {
+//   question6CorrectAnswer = 1;
+// } else {
+//   question6CorrectAnswer = 0;
+// }
 
 let topTenShows = ['The Office', 'Game of Thrones', 'Parks and Rec', 'Severence', 'Lost', 'New Girl', 'Friends', 'The Umbrella Academy', 'Band of Brothers', 'Friday Night Lights'];
 
 
 let guessShowAttempts = 6;
 let userShowGuess;
+let isGuessCorrect = false;
 
-while (guessShowAttempts > 0) {
+while (guessShowAttempts > 0 && !isGuessCorrect) {
   userShowGuess = prompt('What is one of my favorite shows?');
-  let isGuessCorrect = false;
   for (let i = 0; i < topTenShows.length; i++) {
     // console.log(userShowGuess, topTenShows[i], userShowGuess.toLowerCase() === topTenShows[i].toLowerCase());
     if (userShowGuess.toLowerCase() === topTenShows[i].toLowerCase()) {
       isGuessCorrect = true;
+      alert('Correct! That show is in my top ten!');
+      score++;
       break;
     }
-    // userShowGuess = prompt('Try again. What is one of my favorite shows?');
   }
-  if (isGuessCorrect) {
-    alert('Correct! That show is in my top ten!');
-    break;
-  } else {
+  if (!isGuessCorrect) {
+    // break;
     guessShowAttempts--;
     alert(`Incorrect. That is not in my top ten. You have ${guessShowAttempts} attempts remaining.`);
   }
-  // if (!boolValue) {
-  //   break;
-  // }
-  // guessShowAttempts--;
 }
-// if (!guessShowAttempts) {
-//   alert('You have no more attempts remaining');
+
+// let question7CorrectAnswer = 0;
+
+// if (userShowGuess === topTenShows) {
+//   question7CorrectAnswer = 1;
+// } else {
+//   question7CorrectAnswer = 0;
 // }
 
-let question7CorrectAnswer = 0;
-
-if (userShowGuess === topTenShows) {
-  question7CorrectAnswer = 1;
-} else {
-  question7CorrectAnswer = 0;
-}
-
-let score = alert(`You recieved a score of ${question1CorrectAnswer + question2CorrectAnswer + question3CorrectAnswer + question4CorrectAnswer + question5CorrectAnswer + question6CorrectAnswer + question7CorrectAnswer} out of 7 possible points`);
-
-score;
+alert(`You recieved a score of ${score} out of 7 possible points`);
 
 alert('You may now enter my website, ' + userName + '.');
